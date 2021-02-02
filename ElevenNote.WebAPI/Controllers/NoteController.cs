627a5f68b5315cf4097034355ae784691c12b129
anicodesit <ani.kulka@gmail.com>
2021-02-01 20:49:47 -0500

    4.10 NoteEdit Model

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -39,12 +39,25 @@ namespace ElevenNote.WebAPI.Controllers
 
             return Ok();
         }
-
+        [HttpGet]
         public IHttpActionResult Get(int id)
         {
             NoteService noteService = CreateNoteService();
             var note = noteService.GetNoteById(id);
             return Ok(note);
+        }
+        [HttpPut]
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
         }
     }
 }
