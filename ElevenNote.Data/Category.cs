1a26ba5d23e5a796bd5755b5dc63c23e40664a50
anicodesit <ani.kulka@gmail.com>
2021-02-02 09:44:38 -0500

    6.00 Swagger Setup

---
+++ ElevenNote.Data/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public enum Category
+    {
+        Work = 1, 
+        School,
+        Grocery,
+        Recipe,
+        Posh_Listing,
+    } 
+}
